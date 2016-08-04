4234056bef1f9bedf75d003cabb35ab0ea9d2161
Erikas Bulba <b.expire@gmail.com>
2016-08-04 23:28:35 +0300

    Added File
    Added File

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+
+	$max_num = 100;
+	$i = 0;
+	
+	
+	while($i++ < $max_num){
+		
+		// Way one tricky and hacky if number dividable by 3 AND 5
+		if($i % 3 == 0 && $i % 5 == 0){
+			echo "FizzBuzz,";
+		}elseif($i % 3 == 0){
+			echo "Fizz,";
+		}elseif($i % 5 == 0){
+			echo "Buzz,";	
+		}else {
+			echo $i.",";
+		}
+		
+		
+		// Way 2 more clear and logic if number dividable by 15 (3x5)
+		/*
+		if($i % 15){
+			echo "FizzBuzz,";
+		}elseif($i % 3 == 0){
+			echo "Fizz,";
+		}elseif($i % 5 == 0){
+			echo "Buzz,";	
+		}else {
+			echo $i.",";
+		}
+		*/
+	}
+	
+?>
